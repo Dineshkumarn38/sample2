9cdc5ac55bb92bf365780e6526b32b9093a0afa2
Dineshkumarn38 <dineshkumarn38@gmail.com>
2022-12-30 18:16:48 +0530

    Hello Java

---
+++ src/main/java/sample/demo1.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class demo1 {
+
+	private void one() {
+		System.out.println("One");
+	}
+
+	private void two() {
+		System.out.println("Two");
+	}
+
+	public static void main(String[] args) {
+		demo1 z = new demo1();
+
+		z.one();
+		z.two();
+	}
+
+}
